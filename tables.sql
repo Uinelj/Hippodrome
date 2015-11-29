cb1e95ea83d72991c71d25fa1d73fdf32f4518a9
Agurato <vincent1611@hotmail.fr>
2015-11-29 14:46:01 +0100

    Code SQL d'ajout des tables

---
+++ tables.sql
@@ -0,0 +1,62 @@
+CREATE TABLE "Agence" (
+    idAgence SERIAL PRIMARY KEY,
+    nomAgence varchar(80) NOT NULL,
+    adresseAgence varchar(80) NOT NULL
+);
+
+CREATE TYPE TypeEmploye AS ENUM ('commercial', 'mecanicien', 'responsable');
+CREATE TABLE "Employe" (
+    idEmploye SERIAL PRIMARY KEY,
+    nomEmploye varchar(80) NOT NULL,
+    adresseEmploye varchar(80) NOT NULL,
+    dateEmbauche date NOT NULL,
+    type TypeEmploye NOT NULL,
+    idAgence integer REFERENCES "Agence" (idAgence)
+);
+
+CREATE TABLE "RespAgence" (
+    idEmploye integer UNIQUE REFERENCES "Employe" (idEmploye),
+    idAgence integer UNIQUE REFERENCES "Agence" (idAgence),
+    PRIMARY KEY (idEmploye, idAgence)
+);
+
+CREATE TABLE "Modele" (
+    nomModele varchar(20) PRIMARY KEY,
+    marque varchar(20) NOT NULL,
+    tarifJournee money NOT NULL,
+    tarifKilometre money NOT NULL,
+    caution money NOT NULL
+);
+
+CREATE TABLE "Vehicule" (
+    immatriculation varchar(8) PRIMARY KEY,
+    dateAchat date,
+    kilometrage integer,
+    nomModele varchar(20) REFERENCES "Modele" (nomModele),
+    idAgence integer REFERENCES "Agence" (idAgence)
+);
+
+CREATE TABLE "Utilitaire" (
+    immatriculation varchar(8) PRIMARY KEY REFERENCES "Vehicule" (immatriculation),
+    capacite integer,
+    chargeMax integer
+);
+
+CREATE TYPE TypeClient AS ENUM ('entreprise', 'particulier');
+CREATE TABLE "Client" (
+    idClient SERIAL PRIMARY KEY,
+    nomClient varchar(80) NOT NULL,
+    adresseClient varchar(80) NOT NULL,
+    type TypeClient NOT NULL
+);
+
+CREATE TABLE "Location" (
+    idLocation SERIAL PRIMARY KEY,
+    immatriculation varchar(8) NOT NULL REFERENCES "Vehicule" (immatriculation),
+    idClient integer NOT NULL REFERENCES "Client" (idClient),
+    dateLocation date NOT NULL,
+    dateRestitution date,
+    kilometrage integer,
+    etatFinal boolean,
+    tarif money
+);
