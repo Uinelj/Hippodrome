a88332bc59a4ba920885b621766f009a55b7cb13
Agurato <vincent1611@hotmail.fr>
2015-11-29 17:25:34 +0100

    index.php changé et qques requêtes de faites

--- index.php
+++ index.php
@@ -1,3 +1,12 @@
-<?php
-  echo 'coucou';
-?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Requêtes PHP</title>
+    </head>
+    <body>
+        <h1 style="text-align:center;">Requêtes PHP pour le projet de Base de Données</h1>
+        <h3 style="text-align:center;">Réalisé par ABADJI Julien et MONOT Vincent</h3>
+        <p>Requête 9</p>
+        <p>Requête 10</p>
+    </body>
+</html>
--- requetes.sql
+++ requetes.sql
@@ -16,10 +16,10 @@ SELECT * FROM "Location" WHERE immatriculation = (SELECT immatriculation FROM "V
 -- Locations de véhicules utilitaires
 SELECT * FROM "Location" WHERE immatriculation = (SELECT immatriculation FROM "Vehicule" WHERE nomModele = (SELECT nomModele FROM "Modele" WHERE nomModele = (SELECT nommodele FROM "Modele" NATURAL JOIN "Utilitaire")))
 
--- Client ayant loué un véhicule non-utilitaire
+-- Clients ayant loué un véhicule non-utilitaire
 SELECT nomClient, adresseClient FROM "Client" WHERE idClient = (SELECT idClient FROM "Location" WHERE immatriculation = (SELECT immatriculation FROM "Vehicule" WHERE nomModele = (SELECT nomModele FROM "Modele" WHERE nomModele != (SELECT nommodele FROM "Modele" NATURAL JOIN "Utilitaire"))))
 
--- Client ayant loué un véhicule utilitaire
+-- Clients ayant loué un véhicule utilitaire
 SELECT nomClient, adresseClient FROM "Client" WHERE idClient = (SELECT idClient FROM "Location" WHERE immatriculation = (SELECT immatriculation FROM "Vehicule" WHERE nomModele = (SELECT nomModele FROM "Modele" WHERE nomModele = (SELECT nommodele FROM "Modele" NATURAL JOIN "Utilitaire"))))
 
 -- REQUÊTE 1
@@ -32,3 +32,47 @@ SELECT nomClient, adresseClient FROM "Client"
         WHERE immatriculation = (SELECT immatriculation FROM "Vehicule"
             WHERE nomModele = (SELECT nomModele FROM "Modele"
         		WHERE nomModele = (SELECT nomModele FROM "Modele" NATURAL JOIN "Utilitaire"))))
+
+-- REQUÊTE 2
+SELECT nomModele, nomMarque FROM "Modele"
+	WHERE nomModele != (SELECT nomModele FROM "Vehicule"
+		WHERE immatriculation NOT IN (SELECT immatriculation FROM "Location"
+			WHERE idClient NOT IN (SELECT idClient FROM "Client"
+				WHERE type = 'entreprise')))
+
+-- REQUÊTE 3
+SELECT idClient, nomClient FROM "Client"
+	WHERE idClient IN (SELECT idClient FROM "Location"
+		WHERE agenceLocation != agenceRestitution)
+
+-- REQUÊTE 4
+
+
+-- REQUÊTE 5
+
+
+-- REQUÊTE 6
+SELECT nomClient, adresseClient FROM "Client"
+	WHERE idClient = (SELECT idClient FROM "Location"
+		GROUP BY idClient
+		ORDER BY count(*) DESC
+		LIMIT 1)
+
+-- Nombre de locations de plus de 3 jours
+SELECT count(*) FROM "Location"
+	WHERE dateRestitution - dateLocation > 3
+
+-- REQUÊTE 7
+
+
+-- REQUÊTE 8 (pas fini)
+SELECT *, SUM(tarif) FROM "Vehicule" NATURAL JOIN "Location"
+	WHERE kilometrage < 20000
+	AND immatriculation IN (SELECT immatriculation FROM "Location"
+	AND idClient IN (SELECT idClient FROM "Client"
+		WHERE type = 'entreprise')
+
+-- REQUÊTE 9
+
+
+-- REQUÊTE 10
