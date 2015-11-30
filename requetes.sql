41b7824c288bb1f548e4d88c06839a8b719c6b56
Agurato <vincent1611@hotmail.fr>
2015-11-30 17:40:46 +0100

    Plus de requêtes + TODO

--- requetes.sql
+++ requetes.sql
@@ -1,20 +1,20 @@
 -- Modèles de véhicules non-utilitaires
-SELECT * FROM "Modele" WHERE nomModele != (SELECT nommodele FROM "Modele" NATURAL JOIN "Utilitaire")
+SELECT * FROM "Modele" WHERE nomModele NOT IN (SELECT nommodele FROM "Modele" NATURAL JOIN "Utilitaire")
 
 -- Modèles de véhicules utilitaires
-SELECT * FROM "Modele" WHERE nomModele = (SELECT nommodele FROM "Modele" NATURAL JOIN "Utilitaire")
+SELECT * FROM "Modele" WHERE nomModele IN (SELECT nommodele FROM "Modele" NATURAL JOIN "Utilitaire")
 
 -- Véhicules non-utilitaires
-SELECT * FROM "Vehicule" WHERE nomModele = (SELECT nomModele FROM "Modele" WHERE nomModele != (SELECT nommodele FROM "Modele" NATURAL JOIN "Utilitaire"))
+SELECT * FROM "Vehicule" WHERE nomModele IN (SELECT nomModele FROM "Modele" WHERE nomModele NOT IN (SELECT nommodele FROM "Modele" NATURAL JOIN "Utilitaire"))
 
 -- Véhicules utilitaires
-SELECT * FROM "Vehicule" WHERE nomModele = (SELECT nomModele FROM "Modele" WHERE nomModele = (SELECT nommodele FROM "Modele" NATURAL JOIN "Utilitaire"))
+SELECT * FROM "Vehicule" WHERE nomModele IN (SELECT nomModele FROM "Modele" WHERE nomModele IN (SELECT nommodele FROM "Modele" NATURAL JOIN "Utilitaire"))
 
 -- Locations de véhicules non-utilitaires
-SELECT * FROM "Location" WHERE immatriculation = (SELECT immatriculation FROM "Vehicule" WHERE nomModele = (SELECT nomModele FROM "Modele" WHERE nomModele != (SELECT nommodele FROM "Modele" NATURAL JOIN "Utilitaire")))
+SELECT * FROM "Location" WHERE immatriculation IN (SELECT immatriculation FROM "Vehicule" WHERE nomModele IN (SELECT nomModele FROM "Modele" WHERE nomModele NOT IN (SELECT nommodele FROM "Modele" NATURAL JOIN "Utilitaire")))
 
 -- Locations de véhicules utilitaires
-SELECT * FROM "Location" WHERE immatriculation = (SELECT immatriculation FROM "Vehicule" WHERE nomModele = (SELECT nomModele FROM "Modele" WHERE nomModele = (SELECT nommodele FROM "Modele" NATURAL JOIN "Utilitaire")))
+SELECT * FROM "Location" WHERE immatriculation IN (SELECT immatriculation FROM "Vehicule" WHERE nomModele IN (SELECT nomModele FROM "Modele" WHERE nomModele IN (SELECT nommodele FROM "Modele" NATURAL JOIN "Utilitaire")))
 
 -- Clients ayant loué un véhicule non-utilitaire
 SELECT nomClient, adresseClient FROM "Client" WHERE idClient = (SELECT idClient FROM "Location" WHERE immatriculation = (SELECT immatriculation FROM "Vehicule" WHERE nomModele = (SELECT nomModele FROM "Modele" WHERE nomModele != (SELECT nommodele FROM "Modele" NATURAL JOIN "Utilitaire"))))
@@ -35,7 +35,7 @@ SELECT nomClient, adresseClient FROM "Client"
 
 -- REQUÊTE 2
 SELECT nomModele, nomMarque FROM "Modele"
-	WHERE nomModele != (SELECT nomModele FROM "Vehicule"
+	WHERE nomModele NOT IN (SELECT nomModele FROM "Vehicule"
 		WHERE immatriculation NOT IN (SELECT immatriculation FROM "Location"
 			WHERE idClient NOT IN (SELECT idClient FROM "Client"
 				WHERE type = 'entreprise')))
@@ -49,6 +49,27 @@ SELECT idClient, nomClient FROM "Client"
 
 
 -- REQUÊTE 5
+-- Nom du responsable de l'Agence1
+SELECT nomEmploye, nomAgence FROM "Employe" NATURAL JOIN "Agence"
+	WHERE idEmploye IN (SELECT idEmploye FROM "RespAgence"
+		WHERE idAgence IN (SELECT idAgence FROM "Agence"
+			WHERE nomAgence = 'Agence1'))
+
+-- Voitures (non-utilitaires) non disponibles
+SELECT * FROM "Vehicule"
+	WHERE nomModele IN (SELECT nomModele FROM "Modele"
+		WHERE nomModele NOT IN (SELECT nommodele FROM "Modele" NATURAL JOIN "Utilitaire"))
+	AND immatriculation IN (SELECT immatriculation FROM "Location"
+		WHERE dateRestitution IS NULL)
+
+-- Final
+SELECT nomEmploye, nomAgence FROM "Employe" NATURAL JOIN "Agence"
+	WHERE idEmploye IN (SELECT idEmploye FROM "RespAgence"
+		WHERE idAgence IN (SELECT idAgence FROM "Vehicule"
+			WHERE nomModele IN (SELECT nomModele FROM "Modele"
+				WHERE nomModele NOT IN (SELECT nommodele FROM "Modele" NATURAL JOIN "Utilitaire"))
+			AND immatriculation IN (SELECT immatriculation FROM "Location"
+				WHERE dateRestitution IS NULL)))
 
 
 -- REQUÊTE 6
@@ -58,12 +79,23 @@ SELECT nomClient, adresseClient FROM "Client"
 		ORDER BY count(*) DESC
 		LIMIT 1)
 
--- Nombre de locations de plus de 3 jours
+-- REQUÊTE 7
+-- Nombre de locations de plus de 3 jours pour l'agence n°1
 SELECT count(*) FROM "Location"
 	WHERE dateRestitution - dateLocation > 3
+	AND immatriculation IN (SELECT immatriculation FROM "Vehicule"
+		WHERE idAgence = 1)
 
--- REQUÊTE 7
+-- Noms des responsables des agences
+SELECT "Agence".idAgence, nomAgence, nomEmploye, count(immatriculation) FROM "Employe" NATURAL JOIN "Agence" JOIN "Vehicule" ON "Vehicule".idAgence = "Agence".idAgence
+	WHERE idEmploye IN (SELECT idEmploye FROM "RespAgence")
+	AND immatriculation IN (SELECT immatriculation FROM "Location"
+		WHERE dateRestitution - dateLocation > 3)
+	GROUP BY "Agence".idAgence, nomAgence, nomEmploye
 
+SELECT nomModele FROM "Vehicule"
+	GROUP BY nomModele
+	HAVING count(immatriculation) > 3
 
 -- REQUÊTE 8 (pas fini)
 SELECT *, SUM(tarif) FROM "Vehicule" NATURAL JOIN "Location"
